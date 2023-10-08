308dbed2a6822dce9eb4b7b69d22189da9ccb87e
Dnyaneshwar-zukle <93701974+Dnyaneshwar-zukle@users.noreply.github.com>
2023-10-08 19:14:28 +0530

    layout Completed 08/10/2023 19:14

--- frontend/src/components/Charts/Charts.js
+++ frontend/src/components/Charts/Charts.js
@@ -1,45 +1,39 @@
 import React from "react";
-import ImageOne from "../../assets/Vuexy-Vuejs-Admin-Dashboard-Template.png";
+import Intensity from "./chartComponents/Intensity";
+import Country from "./chartComponents/Country";
+import Likehood from "./chartComponents/Likelihood";
+import Relevance from "./chartComponents/Relevance";
+import Year from "./chartComponents/Year";
 
 const Charts = () => {
   return (
     <div>
       <div class="flex mb-4">
         <div class="w-ful">
-          <h1 className="white">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </h1>
+          <Country />
         </div>
       </div>
 
       {/*  Two columns    */}
       <div class="flex gap-5 lg:flex-nowrap flex-wrap  mb-4 2xl:flex-col">
         <div class="w-full lg:w-1/2">
-          <img className="w-full" src={ImageOne} alt="" />
+          <Intensity />
         </div>
         <div class="w-full lg:w-1/2">
-          <img className="w-full" src={ImageOne} alt="" />
+          <Likehood />
         </div>
       </div>
 
       {/*  Three columns    */}
       <div class="flex gap-5 lg:flex-nowrap flex-wrap  mb-4 2xl:flex-col">
         <div class="w-full lg:w-1/3">
-          <img className="w-full" src={ImageOne} alt="" />
+          <Relevance />
         </div>
         <div class="w-full lg:w-1/3">
-          <img className="w-full" src={ImageOne} alt="" />
+          <Year />
         </div>
         <div class="w-full lg:w-1/3">
-          <img className="w-full" src={ImageOne} alt="" />
+          <Intensity />
         </div>
       </div>
     </div>
---
+++ frontend/src/components/Charts/chartComponents/Country.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+
+const Country = () => {
+  // Sample data for the bar chart
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "Sample Data",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <h2>Bar Chart Example</h2>
+      <Doughnut data={data} options={options} />
+    </div>
+  );
+};
+
+export default Country;
